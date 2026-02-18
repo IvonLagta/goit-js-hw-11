b1a0e3fba79a88d8be98febf814ecb14c05c3658
IvonLagta <32240085+IvonLagta@users.noreply.github.com>
2026-02-18 11:59:16 +0100

    Refactor main.js for better image search handling
    Refactor image fetching functionality and improve error messages.

--- src/main.js
+++ src/main.js
@@ -1,30 +1,28 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-import { getImagesByQuery } from './js/pixabay-api.js';
+import { getImagesByQuery } from "./pixabay-api.js";
 import {
   createGallery,
   clearGallery,
   showLoader,
   hideLoader,
-} from './js/render-functions.js';
+} from "./render-functions.js";
 
-const form = document.querySelector('.form');
-const loadMoreBtn = document.querySelector('.load-more');
+const form = document.querySelector(".form");
 
-let currentQuery = '';
+let currentQuery = "";
 let currentPage = 1;
-let totalHits = 0;
 
-form.addEventListener('submit', function (event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  const query = event.currentTarget.elements['search-text'].value.trim();
+  const query = event.currentTarget.elements["search-text"].value.trim();
 
   if (!query) {
     iziToast.warning({
-      message: 'Please enter a search query!',
-      position: 'bottomCenter',
+      message: "Please enter a search query!",
+      position: "bottomCenter",
     });
     return;
   }
@@ -33,63 +31,24 @@ form.addEventListener('submit', function (event) {
   currentPage = 1;
 
   clearGallery();
-  loadMoreBtn.classList.add('hidden');
   showLoader();
 
   getImagesByQuery(currentQuery, currentPage)
     .then(function (data) {
-      totalHits = data.totalHits;
-
       if (data.hits.length === 0) {
         iziToast.info({
-          message: "Sorry, I couldn't find this images.",
-          position: 'bottomCenter',
+          message: "We're sorry, we couldn't find any images.",
+          position: "bottomCenter",
         });
         return;
       }
 
       createGallery(data.hits);
-
-      if (totalHits > 15) {
-        loadMoreBtn.classList.remove('hidden');
-      }
     })
     .catch(function () {
       iziToast.error({
-        message: 'Error fetching images.',
-        position: 'bottomCenter',
-      });
-    })
-    .finally(function () {
-      hideLoader();
-    });
-});
-
-loadMoreBtn.addEventListener('click', function () {
-  currentPage += 1;
-
-  showLoader();
-  loadMoreBtn.classList.add('hidden');
-
-  getImagesByQuery(currentQuery, currentPage)
-    .then(function (data) {
-      createGallery(data.hits);
-
-      const totalLoaded = currentPage * 15;
-
-      if (totalLoaded >= totalHits) {
-        iziToast.info({
-          message: "We're sorry, but you've reached the end of search results.",
-          position: 'bottomCenter',
-        });
-      } else {
-        loadMoreBtn.classList.remove('hidden');
-      }
-    })
-    .catch(function () {
-      iziToast.error({
-        message: "I can't load more images.",
-        position: 'bottomCenter',
+        message: "Error fetching images.",
+        position: "bottomCenter",
       });
     })
     .finally(function () {
