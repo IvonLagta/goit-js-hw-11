315b29030bfbd2d02e6637263b8c9907d1fcf874
IvonLagta <32240085+IvonLagta@users.noreply.github.com>
2026-02-18 12:03:48 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-
-import { getImagesByQuery } from "./pixabay-api.js";
+import { getImagesByQuery } from "./js/pixabay-api.js";
 import {
   createGallery,
   clearGallery,
   showLoader,
   hideLoader,
-} from "./render-functions.js";
+} from "./js/render-functions.js";
 
 const form = document.querySelector(".form");
 
@@ -55,3 +54,4 @@ form.addEventListener("submit", function (event) {
       hideLoader();
     });
 });
+
