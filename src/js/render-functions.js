cc6ff5a2f45d22ff2bfa235b05fad3c67bc6e00f
IvonLagta <32240085+IvonLagta@users.noreply.github.com>
2026-02-18 12:39:18 +0100

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,7 +3,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector(".loader");
-
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
@@ -42,4 +41,5 @@ export function showLoader() {
 export function hideLoader() {
   loader.classList.remove("is-visible");
 }
+
 
